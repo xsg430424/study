23990dac49319902272865f70290edd74bfb7fc1
xiaoshuigeng <742748444@qq.com>
2020-07-08 15:07:02 +0800

    sleuth

---
+++ spring-cloud-demo/spring-cloud-sleuth/zipkin-service/src/main/java/com/forezp/ZipkinServiceApplication.java
@@ -0,0 +1,26 @@
+package com.forezp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import zipkin.server.EnableZipkinServer;
+import zipkin.storage.mysql.MySQLStorage;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
+//@EnableZipkinStreamServer
+public class ZipkinServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinServiceApplication.class, args);
+	}
+
+	@Bean
+	public MySQLStorage mySQLStorage( DataSource datasource) {
+		return MySQLStorage.builder().datasource(datasource).executor(Runnable::run).build();
+	}
+}
